322fb0c8341ffbdbd6a6c0340c188917a3ab7480
Christoph Manske <hathagat@mail.com>
2014-08-05 15:33:53 +0200

    final version

--- interface/KinectSignal.cs
+++ interface/KinectSignal.cs
@@ -9,11 +9,9 @@ using System.IO;
 
 using Microsoft.Kinect;
 
-
-
 namespace KinectTest01 {
 
-    ///Klasse, wo die Signale der Kinect in ein geeignetes Format umgeschrieben werden.
+    ///Klasse, die Signale der Kinect in ein geeignetes Format umgeschreibt.
     class KinectSignal {
         /// <summary>
         /// aktueller Kinect-Sensor
@@ -29,25 +27,25 @@ namespace KinectTest01 {
         const byte CUBE_SIZE = 4;
 
         /// <summary>
-        /// Minimaltiefe, in der Signal erfasst werden soll. Standard: 500? 700
+        /// Minimaltiefe, in der Signal erfasst werden soll. Standard: 500 
         /// </summary>
         int minDepth = 700;
 
         /// <summary>
-        /// Maximaltiefe. Standard: 1000? 850
+        /// Maximaltiefe. Standard: 1000
         /// </summary>
         int maxDepth = 850;
 
         /// <summary>
         /// Gibt an (zwischen 0 und 1), welcher Anteil des gesamten Bildes (vom Zentrum aus gesehen) verarbeitet werden soll.
         /// </summary>
-        double screenRatio = 0.15;//ursprünglich 0.3, 0.2 ist aber ganz gut
+        double screenRatio = 0.15; //Standard: 0.3
 
         bool toPrint = false;
 
         /// <summary>
-        /// Wie lange ein Schleifendurchlauf dauert, bis ein Signal erneut gelesen wird.
-        /// Muss genauso groß wie timePerPattern im .ino-Sketch sein, minimal 12 (flackert zu sehr), kann vielleicht um 1 kleiner sein?
+        /// Gibt an wie lange ein Schleifendurchlauf dauert, bis ein Signal erneut gelesen wird.
+        /// Muss genauso groß wie timePerPattern im .ino-Sketch sein
         /// Gute Werte: 60, 90 (bzw. 59/89)
         /// </summary>
         int delayTime =59;
@@ -75,11 +73,6 @@ namespace KinectTest01 {
         public void readSignal() {
             Console.WriteLine("readSignal wurde betreten!");
 
-            // Look through all sensors and start the first connected one.
-            // This requires that a Kinect is connected at the time of app startup.
-            // To make your app robust against plug/unplug, 
-            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
-
             foreach (var potentialSensor in KinectSensor.KinectSensors) {
                 if (potentialSensor.Status == KinectStatus.Connected) {
                     sensor = potentialSensor;
@@ -90,36 +83,26 @@ namespace KinectTest01 {
 
             if (null != sensor) {
 
-                // Turn on the depth stream to receive depth frames
+                // Aktiviert den Tiefen Stream, nach diesem Aufruf hat die Methode auch die Daten über die Größe des Bildes unter FramePixelDataLength gespeichert
                 sensor.DepthStream.Enable(DepthImageFormat.Resolution80x60Fps30);
-
-                //Nach dieser Zeile hat sensor.DepthStream auch die Daten über die Größe des Bildes unter FramePixelDataLength gespeichert
-
-                // Allocate space to put the depth pixels we'll receive
                 depthPixels = new DepthImagePixel[sensor.DepthStream.FramePixelDataLength];
 
-                //Addition eines Eventhandlers (a+=b) sieht komisch aus, aber das heisst, dass eine Kausalbeziehung zwischen a und b hergestellt wird;
+                //Addition eines Eventhandlers (a+=b) bedeutet, dass eine Kausalbeziehung zwischen a und b hergestellt wird.
                 //a -> b, d.h. wenn a aufgerufen wird, wird auch b aufgerufen
-                //hier heisst es: "Wenn sensor einen depth-frame bereit hat, rufe die Methode VerarbeiteSensorFrameDaten auf"
-                //Somit die wichtigste Stelle in dieser Methode, da hier die Aufrufe der Tiefensensor-Methoden erfolgen
-                //Der Methodenaufruf hat hier an dieser Stelle keine Argumente, aber diese werden automatisch übergeben? (s. die Methode SensorDepthFrameReady selbst,
-                //die 2 Argumente hat
-
+                //hier bedeutet es: "Wenn sensor einen depth-frame bereit hat, rufe die Methode VerarbeiteSensorFrameDaten auf"
                 sensor.DepthFrameReady += VerarbeiteSensorFrameDaten;
 
-
                 port.Open();
-                //Sensor starten. So ruft dann der Sensor von selbst Daten und somit die verbundenen Methoden auf
+             
+				//Sensor starten. So ruft dann der Sensor von selbst Daten und somit die verbundenen Methoden auf
                 try {
                     sensor.Start();
                 } catch (IOException e) {
                     Console.WriteLine("IOException! " + e);
-                    
                     sensor = null;
                 }
             } else {
                 Console.WriteLine("sensor ist null");
-
             }
             Console.WriteLine("ReadSignal verlassen!");
         }
@@ -130,70 +113,44 @@ namespace KinectTest01 {
         /// <param name="sender">Objekt, dass Methode aufruft?</param>
         /// <param name="e">Das Signal, wird automatisch beim Callback übergeben.</param>
         private void VerarbeiteSensorFrameDaten(object sender, DepthImageFrameReadyEventArgs e) {
-            /*
+            
+			short[,] depthValues2D = new short[bildHoehe, bildHoehe]; //1. Index: Zeile, 2. Index: Spalte
+            short[,] depthValues2DSmall;
+            short[,] depthValuesFiltered;
+            String depthValuesString;
+            
+			/*
              using (Variablendeklaration) heisst, dass nur für diesen Block die Variable deklariert wird, also nur in diesem Block
              * existiert sie und am Ende des Blocks wird sie -automatisch- freigemacht/entfernt, ohne dass man das von Hand machen muss.
              * 
              * e ist ein Argument dieser Funktion und OpenDepthImageFrame() gibt das Signal in e zurück, dass automatisch beim
-             * Aufruf als Argument e übergeben wird, also ist da drin das Signal?
+             * Aufruf als Argument e übergeben wird
              * 
             */
- 
-            short[,] depthValues2D = new short[bildHoehe, bildHoehe]; //1. Index: Zeile, 2. Index: Spalte
-            short[,] depthValues2DSmall;
-            short[,] depthValuesFiltered;
-            String depthValuesString;
-            //Console.Write(">"+counater+ " ");
-            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame()) {
-                //Console.Write("a");
-                //wenn der Speicher für das aktuelle Signal nicht leer ist (also vorliegt)
-                if (depthFrame != null) {
-                   
+						
+			using (DepthImageFrame depthFrame = e.OpenDepthImageFrame()) {
+                
+				if (depthFrame != null) {
+                 
                     //Pixeldaten des aktuellen Frames kopieren aus depthFrame in depthPixels
                     //depthPixels hat danach die Pixel-Daten aus dem Signal, das von e bzw. depthFrame kommt
                     //(depthPixels ist ein Array, hat also die Daten von allen Pixeln!)
                     depthFrame.CopyDepthImagePixelDataTo(depthPixels);
-                    //Console.WriteLine("Laenge: " + depthPixels.Length + "  PixelDataLength: " + depthFrame.PixelDataLength);
                     
-                    //Console.WriteLine("Ausgabe kommt gleich!" + depthPixels[0].Depth);
-
-                    //Das Bild muss quadratisch gemacht werden, hier durch Abschneiden der Ränder
-                    //Kopieren in ein zweidimensionales Array
-
-                    //Der Aufbau des Arrays ist aber ein bisschen komisch, es läuft irgendwie von oben nach unten und von rechts (!) nach links
-                    //Deswegen das Füllen von depthValues2D etwas anders, und zwar läuft die innere Schleife von bildHoehe-1 bis 0 und nicht umgekehrt
-                    //Wenn wieder alter Zustand hergestellt werden soll, Grenzen anpassen
-                    /*
-                    for (int i = 0; i < bildHoehe; i++) {
-                        counter += (bildBreite - bildHoehe) / 2;
-                        //for (int j = bildHoehe-1; j >=0; j--) {
-                        for (int j = 0; j < bildHoehe; j++) {
-                            //hier das Abschneiden
-                            depthValues2D[i, j] = depthPixels[counter].Depth;
-                            // Console.Write("{0,3}",depthValues2D[i,j]/100 + " ");
-                            counter++;
-                        }
-                        // Console.WriteLine();
-                        counter += (bildBreite - bildHoehe) / 2;
-                    }
-                    */
-
-                    //Kopieren in ein zweidimensionales Array
                     //Das Bild muss quadratisch gemacht werden, hier durch Abschneiden der Ränder, was in der Methode passiert.
                     //Der Würfel muss dabei mit der Rückseite (gegenüber dem Anschluss) zum Betrachter zeigen, damit die Richtungen stimmen.
                     depthValues2D = GetQuadraticDepthValuesArray(depthPixels);
                     
+					// Die Variable toPrint befindet sich im Kopf dieser Datei und wurde zu Debugging Zwecke verwendet.
+					// Standard : false, also keine Testausgaben
                     if (toPrint) {
-                        //PrintDepthData(depthValues2D);
-
+                    
                         screenRatio = 0.2;
                         depthValues2DSmall = CropDepthData(depthValues2D, screenRatio);
-                        //Console.WriteLine("Abgeschnitten: ");
                         PrintDepthData(depthValues2DSmall);
 
                         depthValues2DSmall = ShrinkingBySkipping2D(depthValues2DSmall, CUBE_SIZE);
                         Console.WriteLine("Verkleinert:");
-                        // PrintDepthData(depthValues2DSmall);
                         Console.WriteLine();
                         depthValuesFiltered = FilterAndResizeNumbers(depthValues2DSmall, minDepth, maxDepth);
 
@@ -203,46 +160,17 @@ namespace KinectTest01 {
                         depthValuesFiltered = SwitchColumns(depthValuesFiltered);
 
                         Console.WriteLine("Vertauscht:");
-                        //PrintDepthData(depthValuesFiltered);
 
                         Console.WriteLine();
                         depthValuesString = Convert2DToString(depthValuesFiltered);
                         Console.WriteLine("Finaler String: " + depthValuesString);
 
                         SendDataString(depthValuesString);
-
-
                         Console.Clear();
                     } else {
-                        //Kürzere Fassung, wo alle Funktionen hintereinander aufgerufen werden
-                        //(Wobei hier deutlich wird, dass man hier viel Aufwand sparen kann, in dem man alle Methoden zusammenfasst, sodass
-                        //das 2D-Tiefendaten-Array nicht immer in den Stack kopiert / aus dem Stack rausgeholt werden muss)
-
-                        /*Einrückungen sehen komisch aus, aber die zeigen, zu welcher Funktion welches Argument gehört:
-                         * in einer Einrückungsstufe sind die Argumente der -darüberliegenden- Funktion enthalten
-                        */
-                        /* short[,] depthValuesTest = FilterAndResizeNumbers(
-                                         ShrinkingBySkipping2D(
-                                             CropDepthData(
-                                                 depthValues2D,
-                                                 screenRatio
-                                             ),
-                                             CUBE_SIZE),
-                                         minDepth,
-                                         maxDepth
-                                     );
-                         PrintDepthData(depthValuesTest);
-                         SendDataString(
-                            Convert2DToString(
-                                 SwitchColumns(
-                                     depthValuesTest
-                                 )
-                             )
-                         );*/
-                        
                         SendDataString(
                            Convert2DToString(
-                            //SwitchColumns(
+                            
                                     FilterAndResizeNumbers(
                                         ShrinkingBySkipping2D(
                                             CropDepthData(
@@ -253,33 +181,15 @@ namespace KinectTest01 {
                                         minDepth,
                                         maxDepth
                                     )
-                            //)
+                          
                             )
                         );
-                       
-                       
-                        //this.TurnSensorOff();
-
                     }
-                    /*
-                    Console.WriteLine("--Ende der Methode. Druecke Enter zum nochmaligen Lesen der Daten oder gebe exit ein zum Beenden.--");
-                    if (Console.ReadLine().CompareTo("asdf") == 0) {
-                        Console.WriteLine("Kinectsensor ausgeschaltet!");
-                        sensor.Stop();
-                    } else {
-                        //sensor.DepthFrameReady += VerarbeiteSensorFrameDaten;
-                    }
-                     * */
                     System.Threading.Thread.Sleep(delayTime);
-                   
-
-
                 } else {
                     Console.WriteLine("Es ist kein Signal in VerarbeiteSensorFrameDaten reingekommen!");
                 }
             }
-            //Console.Write("<\n");
-            //counater++;
         }
 
         /// <summary>
@@ -302,13 +212,9 @@ namespace KinectTest01 {
             for (int i = 0; i < bildHoehe; i++) {
                 counter += (bildBreite - bildHoehe) / 2;
                 for (int j = bildHoehe-1; j >=0; j--) {
-                //for (int j = 0; j < bildHoehe; j++) {
-                    //hier das Abschneiden
                     result[i, j] = depthPixels[counter].Depth;
-                    // Console.Write("{0,3}",depthValues2D[i,j]/100 + " ");
                     counter++;
                 }
-                // Console.WriteLine();
                 counter += (bildBreite - bildHoehe) / 2;
             }
             return result;
@@ -366,13 +272,9 @@ namespace KinectTest01 {
 
                 for (int i = 0; i < goalSize; i++) {
                     innerChosenPixel = Math.Ceiling((i + 0.5) * (data.GetLength(0) / goalSize) + (divisible ? 0 : (i % 2)));
-
-                    // Console.WriteLine(data.GetLength(0));
-                    //Console.WriteLine("Zuweisung: " + data[(int)outerChosenPixel, (int)innerChosenPixel] + ": " + (int)innerChosenPixel + " " + (int)outerChosenPixel);
                     result[j, i] = data[(int)outerChosenPixel, (int)innerChosenPixel];
                 }
             }
-
             return result;
         }
 
@@ -399,7 +301,6 @@ namespace KinectTest01 {
 
                 }
             }
-           
             return result;
         }
 
@@ -433,7 +334,6 @@ namespace KinectTest01 {
                     result[j, (i == 3) ? 0 : (i + 1)] = data[j, i];
                 }
             }
-
             return result;
         }
 
@@ -476,19 +376,9 @@ namespace KinectTest01 {
         /// <param name="data">Zu sendende Daten als String</param>
         public void SendDataString(String data) {
 
-            //SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
-
             try {
-               // port.Open();
-
                 //Sende Datenstring mit x als Trennzeichen für die einzelnen Blöcke
                 port.Write(data+"x");
-
-                //nach langer Suche und vielen Testausgaben endlich die Problemstelle gefunden!
-                //hier bleibt das Programm manchmal hängen: es wird immer b ausgegeben, c aber nicht mehr, wenn es hängenbleibt
-                //Console.Write("b");
-                //port.Close();
-                //Console.Write("c");
 
             } catch (Exception e) {
                 Console.WriteLine("Es ist ein Problem aufgetreten.");
@@ -497,9 +387,8 @@ namespace KinectTest01 {
                 //auf Enter warten und dann Programm schliessen
                 Console.ReadLine();
                 System.Environment.Exit(2);
-               
             }
-            
+          
         }
 
         /// <summary>
@@ -510,8 +399,6 @@ namespace KinectTest01 {
             sensor.DepthFrameReady -= VerarbeiteSensorFrameDaten;
             port.Close();
             sensor.Stop();
-            
-           
         }
 
         /// <summary>
@@ -527,25 +414,12 @@ namespace KinectTest01 {
                 {4,3,2,1}
             };
 
-            
-            
             for (int i = 0; i < 100; i++) {
                 tempString = Convert2DToString(SwitchColumns(depthValues));
                 Console.WriteLine(tempString);
                 SendDataString(tempString);
                 System.Threading.Thread.Sleep(zeit);
                 SendDataString("x");
-
-                /*
-                SendDataString("4444333322221111x");
-                System.Threading.Thread.Sleep(zeit);
-                SendDataString("3333333333333333x");
-                System.Threading.Thread.Sleep(zeit);
-                SendDataString("2222222222222222x");
-                System.Threading.Thread.Sleep(zeit);
-                SendDataString("1111111111111111x");
-                System.Threading.Thread.Sleep(zeit);
-                 */
             }
             Console.WriteLine("Ende");
         }
